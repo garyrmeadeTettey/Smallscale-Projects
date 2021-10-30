172982a4e7e1c2619bf6fc567bb5168ba6f2ac6d
Gary_Ross Meade Tettey <82911272+garyrmeadeTettey@users.noreply.github.com>
2021-10-30 13:11:14 +0200

    C# code challenges
    Build File

---
+++ C# Code challenge/ExponentialCalculation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {   
+        static void Main(strings args)
+        { Console.WriteLine("Enter base number");
+            Base = Convert.ToInt32(Console.ReadLine());
+            int Base; 
+            int power;
+            int result;
+
+            Console.WriteLine("Enter base number");
+            Base = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter power number");
+            power = Convert.ToInt32(Console.ReadLine());
+
+            result = Calc(Base, power);
+
+        static void Calc(int Base, int power)
+            {
+                if (power != 0)
+                {
+                    return Base * Calc(Base, power);
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ C# Code challenge/SumUserInput/Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please a number to sum to");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int Sum = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                Sum += 1;
+            }
+
+            Console.WriteLine("Sum = ", Sum);
+            Console.ReadKey();
+        }
+    }
+}
